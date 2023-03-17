6229e807b2742a07d44f26133563885cb21c499f
FrankMugagga <frank.mugagga@gmail.com>
2023-03-17 11:31:15 +0300

    Test the function addTodo and deleteTodoItem to confirm their expectations

---
+++ src/todo.test.js
@@ -0,0 +1,72 @@
+import { JSDOM } from 'jsdom';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+const html = readFileSync(resolve(__dirname, './index.html'), 'utf8');
+const { window } = new JSDOM(html);
+const { document } = window;
+const todoList = document.getElementsByClassName('todo-list')[0];
+let todoItems = [];
+const addTodo = (todo) => {
+  const todolistObj = {
+    description: todo,
+    completed: false,
+    index: todoItems.length,
+  };
+  todoItems.push(todolistObj);
+};
+const deleteTodoItem = () => {
+  todoItems = todoItems.filter((item) => item.completed === false);
+};
+describe('Todo List', () => {
+  beforeEach(() => {
+    todoItems = [];
+  });
+  describe('addTodo', () => {
+    it('should add a todo to the list', () => {
+      addTodo('Test todo');
+      expect(todoItems.length).toEqual(1);
+      expect(todoItems[0].description).toEqual('Test todo');
+      expect(todoItems[0].completed).toBeFalsy();
+      expect(todoItems[0].index).toEqual(0);
+    });
+    it('should add multiple todos to the list', () => {
+      addTodo('Test todo 1');
+      addTodo('Test todo 2');
+      addTodo('Test todo 3');
+      expect(todoItems.length).toEqual(3);
+      expect(todoItems[0].description).toEqual('Test todo 1');
+      expect(todoItems[1].description).toEqual('Test todo 2');
+      expect(todoItems[2].description).toEqual('Test todo 3');
+    });
+  });
+  describe('deleteTodoItem', () => {
+    it('should delete a completed todo from the list', () => {
+      addTodo('Test todo 1');
+      addTodo('Test todo 2');
+      addTodo('Test todo 3');
+      todoItems[1].completed = true;
+      deleteTodoItem();
+      expect(todoItems.length).toEqual(2);
+      expect(todoItems[0].description).toEqual('Test todo 1');
+      expect(todoItems[1].description).toEqual('Test todo 3');
+    });
+    it('should not delete any todos if none are completed', () => {
+      addTodo('Test todo 1');
+      addTodo('Test todo 2');
+      addTodo('Test todo 3');
+      deleteTodoItem();
+      expect(todoItems.length).toEqual(3);
+      expect(todoItems[0].description).toEqual('Test todo 1');
+      expect(todoItems[1].description).toEqual('Test todo 2');
+      expect(todoItems[2].description).toEqual('Test todo 3');
+    });
+    it('should not delete any todos if there are no todos', () => {
+      deleteTodoItem();
+      expect(todoItems.length).toEqual(0);
+    });
+  });
+  it('should retrieve the todo list element from the DOM', () => {
+    expect(todoList.tagName).toEqual('UL');
+    expect(todoList.className).toEqual('todo-list');
+  });
+});
